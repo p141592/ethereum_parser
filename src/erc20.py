e3bfa4fbbad300456a54ee22e27e23ef856c318c
Nikolay Baryshnikov <root@k0d.ru>
2018-12-11 00:29:21 +0300

    WIP: use web3py

--- src/erc20.py
+++ src/erc20.py
@@ -1,43 +1,70 @@
-TMP_METHODS = set()
+import json
+
+from web3.auto import w3
+
+from tools import toDict
+
+ERC20_ABI = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],'
+                       '"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{'
+                       '"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve",'
+                       '"outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable",'
+                       '"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"",'
+                       '"type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},'
+                       '{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to",'
+                       '"type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{'
+                       '"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},'
+                       '{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],'
+                       '"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{'
+                       '"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"",'
+                       '"type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},'
+                       '{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],'
+                       '"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{'
+                       '"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer",'
+                       '"outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable",'
+                       '"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},'
+                       '{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"",'
+                       '"type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},'
+                       '{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},'
+                       '{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value",'
+                       '"type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{'
+                       '"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender",'
+                       '"type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval",'
+                       '"type":"event"}]')
 
 
-def empty(data):
-    return None
+async def parse_transaction(transaction):
+    # Получили транзакцию
+    # transaction = w3.eth.getTransaction('0x7e84c4538e056f03ab689d1b0b112b47921b2f82fea0396bfa38194b6461ffdd')
+    if not w3.toText(w3.toText(transaction.input)):
+        # Проверить input_data
+        return json.dumps(transaction, cls=toDict)
+        # -> Вернуть результат
+
+    # Если есть:
+    # По адресу получателя получить контракт
+    erc20 = await get_erc20_contract(transaction.to)
+    # Проверить на erc20
+    # Спарсить input_data
+
+    return await parse_input_data(erc20, transaction.to)
 
 
-def unknown(data):
-    return data
+async def parse_input_data(contract, transaction):
+    return dict(
+        function=contract.decode_function_input(transaction.input)[0].fn_name(),
+        data=contract.decode_function_input(transaction.input)[1]
+    )
 
 
-def transfer(data): # Transfer given number of tokens from message sender to given recipient
-    pass
+async def get_erc20_contract(address):
+    return await w3.eth.contract(address=address, abi=ERC20_ABI)
 
 
-def get_method(data):
-    _method = data[:10]
-    return _method if _method in METHODS_MAP else 'unknown'
-
-
-METHODS_MAP = {
-    'unknown': unknown,
-    '0x': empty,
-    '0xa9059cbb': transfer,  # allowance
-    #'0x9e281a98': withdrawToken,  # approve
-    # '0xa9059cbb': '',  # transferFrom
-    # '0xa9059cbb': '',  # transfer
-    # '0xa9059cbb': '',  # balanceOf
-    # '0xa9059cbb': '',  # totalSupply
-    # '0xa9059cbb': '',  # decimals
-    # '0xa9059cbb': '',  # symbol
-    # '0xa9059cbb': ''  # name
-}
-
-
-def decode_contract_data(raw_input):
-    _method = get_method(raw_input)
-    _data = METHODS_MAP.get(_method)(raw_input)
-    if _data:
-        return dict(
-            method=_method,
-            data=_data
-        )
+async def parse_erc20(address):
+    erc20 = await get_erc20_contract(address)
+    return dict(
+        name=await erc20.functions.name().call(),
+        symbol=await erc20.functions.symbol().call(),
+        decimals=await erc20.functions.decimals().call(),
+        totalSupply=await erc20.functions.totalSupply().call()
+    )
--- src/tools.py
+++ src/tools.py
@@ -1,8 +1,20 @@
-import asyncio
+def toDict(dictToParse):
+    # convert any 'AttributeDict' type found to 'dict'
+    parsedDict = dict(dictToParse)
+    for key, val in parsedDict.items():
+        if 'list' in str(type(val)):
+            parsedDict[key] = [_parseValue(x) for x in val]
+        else:
+            parsedDict[key] = _parseValue(val)
+    return parsedDict
 
 
-async def speedtest(loop, _len):
-    while True:
-        _tmp = _len
-        await asyncio.sleep(1)
-        print(_len - _tmp)
+def _parseValue(val):
+    # check for nested dict structures to iterate through
+    if 'dict' in str(type(val)).lower():
+        return toDict(val)
+    # convert 'HexBytes' type to 'str'
+    elif 'HexBytes' in str(type(val)):
+        return val.hex()
+    else:
+        return val
